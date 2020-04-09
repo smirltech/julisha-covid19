36d7a824371942eaed30064e232cb5c5bdd8f63c
marien <marien@smirl.org>
2020-04-09 18:43:54 +0200

    - Hide Toast on net available

--- app/src/main/java/org/smirl/julisha/IntroActivity.java
+++ app/src/main/java/org/smirl/julisha/IntroActivity.java
@@ -1,87 +1,96 @@
 package org.smirl.julisha;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
 import org.smirl.julisha.core.*;
+import org.smirl.julisha.core.volley.MyStringRequest;
+import org.smirl.julisha.core.volley.StaticRequestQueue;
 
+import java.io.File;
 import java.io.IOException;
-
 
 public class IntroActivity extends AppCompatActivity implements Constants {
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_intro);
+        populateProvinces();
+        populateVilles();
+
+        if (Utilities.checkInternetAvailable(this)) {
+           // Utilities.toastIt(this, "Network connection available!");
+            populateCases();
+        } else {
+            Utilities.toastIt(this, "No network connection!");
+            DataUpdater.populateLocalCases(this, new DataUpdater.UpdaterListener() {
+                @Override
+                public void onCompleted() {
+                    toNextActivity();
+                }
+
+                @Override
+                public void onFailed() {
+                    toNextActivity();
+                }
+            });
+        }
+    }
 
 
-    populateProvinces();
-    populateVilles();
-
-    if (Utilities.checkInternetAvailable(this)) {
-    //  Utilities.toastIt(this, "Network connection available!");
-      populateCases();
-    } else {
-      Utilities.toastIt(this, "Pas de connexion!");
-      DataUpdater.populateLocalCases(this, new DataUpdater.UpdaterListener() {
-        @Override
-        public void onCompleted() {
-          toNextActivity();
+    private void populateProvinces() {
+        String dd = null;
+        try {
+            dd = FileManager.readFromAssets(this, "rdc/provinces.json");
+            Julisha.loadProvinces(dd);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        @Override
-        public void onFailed() {
-          toNextActivity();
+    }
+
+    private void populateVilles() {
+        String dd = null;
+        try {
+            dd = FileManager.readFromAssets(this, "rdc/villes.json");
+            Julisha.loadVilles(dd);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      });
-    }
-  }
 
-
-  private void populateProvinces() {
-    String dd = null;
-    try {
-      dd = FileManager.readFromAssets(this, "rdc/provinces.json");
-      Julisha.loadProvinces(dd);
-    } catch (IOException e) {
-      e.printStackTrace();
     }
 
-  }
+    private void populateCases() {
 
-  private void populateVilles() {
-    String dd = null;
-    try {
-      dd = FileManager.readFromAssets(this, "rdc/villes.json");
-      Julisha.loadVilles(dd);
-    } catch (IOException e) {
-      e.printStackTrace();
+        DataUpdater.populateCases(this, new DataUpdater.UpdaterListener() {
+            @Override
+            public void onCompleted() {
+                toNextActivity();
+            }
+
+            @Override
+            public void onFailed() {
+                toNextActivity();
+            }
+        });
     }
 
-  }
-
-  private void populateCases() {
-
-    DataUpdater.populateCases(this, new DataUpdater.UpdaterListener() {
-      @Override
-      public void onCompleted() {
-        toNextActivity();
-      }
-
-      @Override
-      public void onFailed() {
-        toNextActivity();
-      }
-    });
-  }
-
-  private void toNextActivity() {
-    startActivity(new Intent(IntroActivity.this, MainActivity.class));
-    finish();
-  }
+    private void toNextActivity() {
+        startActivity(new Intent(IntroActivity.this, MainActivity.class));
+        finish();
+    }
 
 
 }
